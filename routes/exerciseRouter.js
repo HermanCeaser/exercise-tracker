eaff4b4aa93997b8d440ac74ab90b13b9b18c3ef
Herman Ceaser <ceaserbanks09@gmail.com>
2020-09-10 12:18:09 +0300

    Able to retrieve exercise for a given user

--- routes/exerciseRouter.js
+++ routes/exerciseRouter.js
@@ -22,40 +22,51 @@ const router = (model) => {
       const { username } = req.body;
       model.User.create({ name: username }, (err, user) => {
         if (err) return console.log(`Failed to create User due to : ${err}`);
-        res.json({ username: user.name, id: user._id });
+        res.json({ username: user.name, _id: user._id });
       });
     }
   });
 
   // Add an Exercise for a user to the database
   exerciseRouter.post("/add", (req, res) => {
-    // validateDate();
+    // Check if Input parameters are not Empty
     if (req.body.userId && req.body.description && req.body.duration) {
       const { userId, description, duration, date } = req.body;
-      model.User.findById(userId, (err, status) => {
-        if (err) return console.log("User does not exist");
-        model.Exercise.create(
-          {
-            userId: userId,
+      //Check if user Exists in the Database
+      const getUser = model.User.findById(userId);
+      getUser // Create an Exercise Based on the passed User Id
+        .then((user) => {
+          return model.Exercise.create({
+            userId: user._id,
             description: description,
             duration: parseInt(duration),
             date: date || new Date(),
-          },
-          (err, data) => {
-            if (err) return console.log(err);
-            console.log(data);
-            res.json({
-              _id: data._id,
-              userId: data.userId,
-              date: data.date.toDateString(),
-              duration: data.duration,
-              description: data.description,
-            });
-          }
-        );
-      });
+          });
+        })
+        // Get the corresponding Username from User's Model
+        .then((CreatedExercise) => {
+          // console.log(CreatedExercise);
+          return model.Exercise.findById(CreatedExercise._id).populate(
+            "userId"
+          );
+        })
+        // Return Populated Exercise Object with Username
+        .then((data) => {
+          // console.log(data);
+          res.json({
+            _id: data._id,
+            username: data.userId.name,
+            date: data.date.toDateString(),
+            duration: data.duration,
+            description: data.description,
+          });
+        })
+        // Catch any errors and pass them to the
+        .catch((err) => {
+          res.status(500).json({ error: err });
+        });
     } else {
-      return res.json({
+      return res.status(500).json({
         error: "userId, description and duration are all required",
       });
     }
